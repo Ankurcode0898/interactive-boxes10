618faf237935f9e32d2e055b03aaa9346cadac31
Ankurcode0898 <ankurverma0898@gmail.com>
2025-05-19 03:44:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 <script>
   function selectBox(box, price) {
-    // Deselect all boxes
+
     document.querySelectorAll('.box').forEach(b => {
       b.classList.remove('selected');
       b.querySelector('input[type="radio"]').checked = false;
     });
 
-    // Select the clicked box
     box.classList.add('selected');
     box.querySelector('input[type="radio"]').checked = true;
 
-    // Update total price
+   
     document.getElementById('total').textContent = price.toFixed(2);
   }
 </script>
